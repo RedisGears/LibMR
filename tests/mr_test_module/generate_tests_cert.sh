13506f1f413842e5591f89fe93d0904b9abfab36
meir <meir@redis.com>
2022-02-02 21:38:54 +0200

    fix test

--- tests/mr_test_module/generate_tests_cert.sh
+++ tests/mr_test_module/generate_tests_cert.sh
@@ -18,7 +18,7 @@ openssl req \
 openssl genrsa -out redis.key 2048
 openssl req \
     -new -sha256 \
-    -key redis.key \ #-passin pass:foobar \
+    -key redis.key \
     -subj '/O=Redis Test/CN=Server' | \
     openssl x509 \
         -req -sha256 \
