e5a41e1e007b74022c939d4edf55321a0bd388f2
Meir Shpilraien (Spielrein) <meir@redis.com>
2022-11-20 09:24:22 +0200

    Update license (#29)
    * Update license
    
    * CI fixes
    
    * CI fixes (2)
    
    * Added license to all files.
    
    Co-authored-by: rafie <rafi@redislabs.com>

--- src/mr_memory.h
+++ src/mr_memory.h
@@ -1,8 +1,8 @@
+
 /*
- * mr_memory.h
- *
- *  Created on: Aug 25, 2021
- *      Author: root
+ * Copyright Redis Ltd. 2021 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
  */
 
 #ifndef SRC_MR_MEMORY_H_
