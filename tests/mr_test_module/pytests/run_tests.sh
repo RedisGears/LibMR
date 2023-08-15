bd8b8eb662b90294e90a23d4fe7a81e9a34a597e
Meir Shpilraien <meir@redis.com>
2023-08-15 15:13:32 +0300

    Added verbose logs on failure

--- tests/mr_test_module/pytests/run_tests.sh
+++ tests/mr_test_module/pytests/run_tests.sh
@@ -19,4 +19,4 @@ else
 fi
 
 
-python3 -m RLTest --module $MODULE_PATH --clear-logs "$@" --module-args "password" --oss_password "password"
+python3 -m RLTest --module $MODULE_PATH --clear-logs "$@" --module-args "password" --oss_password "password" --verbose-information-on-failure
