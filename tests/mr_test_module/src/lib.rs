6885961f55ed53e88cf8b6d1c00ca532c2e6dc85
Meir Shpilraien <meir@redis.com>
2023-08-17 11:32:26 +0300

    Increase max idle time on replace_keys_values

--- tests/mr_test_module/src/lib.rs
+++ tests/mr_test_module/src/lib.rs
@@ -267,6 +267,8 @@ fn replace_keys_values(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
         .create_execution()
         .map_err(|e| RedisError::String(e))?;
 
+    execution.set_max_idle(100000);
+
     let blocked_client = ctx.block_client();
     execution.set_done_hanlder(|mut res, mut errs| {
         let thread_ctx = ThreadSafeContext::with_blocked_client(blocked_client);
