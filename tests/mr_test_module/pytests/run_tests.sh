d4818db044bdcff86c4d32a4e7fc9475e7394afc
Victor Polevoy <fx@thefx.co>
2024-12-04 13:36:42 +0100

    Revert the absence of the no-progress flag for RLTest

--- tests/mr_test_module/pytests/run_tests.sh
+++ tests/mr_test_module/pytests/run_tests.sh
@@ -19,4 +19,4 @@ else
 fi
 
 
-python3 -m RLTest --no-progress --verbose-information-on-failure --module $MODULE_PATH --clear-logs "$@" --oss_password "password"
+python3 -m RLTest --verbose-information-on-failure --module $MODULE_PATH --clear-logs "$@" --oss_password "password"
