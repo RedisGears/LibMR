1c1ed1f75a6b9ca6ecee784041f8d333689ca156
Tom Gabsow <gabsow.tom@gmail.com>
2025-12-07 15:17:28 +0200

    tests(macos): minimal fixes to run tests locally\n\n- Use .dylib on macOS in test module Makefile\n- Randomize ports in RLTest runner to avoid conflicts\n- Add minimal test-local Cargo env for libclang & OpenSSL\n- Revert Cargo.toml to original dep features (no feature toggles)\n\nNo changes to submodules or deps; build artifacts not committed.

--- tests/mr_test_module/pytests/run_tests.sh
+++ tests/mr_test_module/pytests/run_tests.sh
@@ -19,4 +19,4 @@ else
 fi
 
 
-python3 -m RLTest --verbose-information-on-failure --no-progress --module $MODULE_PATH --clear-logs "$@" --oss_password "password" --enable-debug-command
+python3 -m RLTest --verbose-information-on-failure --no-progress --randomize-ports --module $MODULE_PATH --clear-logs "$@" --oss_password "password" --enable-debug-command
